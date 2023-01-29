f61b498596ef8a82502bb8fd7caf230db3080ee4
Kirill M <marulin@live.ru>
2023-01-29 21:59:51 +0300

    start init

--- src/index.js
+++ src/index.js
@@ -1,14 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./style.css";
+import "./style.scss";
+import FilterButtons from "./buttons/filterButtons";
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  
-    <App/>
-  
-);
-
-
-
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<FilterButtons />);
