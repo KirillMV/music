eed96840e78eded5e6a68eb08fa11acafbf1d9b8
Kirill M <marulin@live.ru>
2023-01-31 14:00:40 +0300

    burger_good

--- src/companents/burger.js
+++ src/companents/burger.js
@@ -1,11 +1,10 @@
 import React from "react";
 import "../companents_style.css";
 
-
 const { useState } = React;
 
 const Burger = () => {
-  const [visible, setVisible] = useState(true);
+  const [visible, setVisible] = useState(false);
 
   const toggleVisibility = () => setVisible(!visible);
 
@@ -15,30 +14,34 @@ const Burger = () => {
         <img className="logo__image" src="img/logo.png" alt="logo"></img>
       </div>
       <div className="nav__burger burger" onClick={toggleVisibility}>
-        {visible && <div className="nav__menu menu"></div>}
+       
 
         <span className="burger__line"></span>
         <span className="burger__line"></span>
         <span className="burger__line"></span>
-
-        <ul className="menu__list">
-          <li className="menu__item">
-            <a href="http://" className="menu__link">
-              Главное
-            </a>
-          </li>
-          <li className="menu__item">
-            <a href="http://" className="menu__link">
-              Мой плейлист
-            </a>
-          </li>
-          <li className="menu__item">
-            <a href="http://" className="menu__link">
-              Войти
-            </a>
-          </li>
-        </ul>
       </div>
+
+      {visible && (
+        <div className="nav__menu menu">
+          <ul className="menu__list">
+            <li className="menu__item">
+              <a href="http://" className="menu__link">
+                Главное
+              </a>
+            </li>
+            <li className="menu__item">
+              <a href="http://" className="menu__link">
+                Мой плейлист
+              </a>
+            </li>
+            <li className="menu__item">
+              <a href="http://" className="menu__link">
+                Войти
+              </a>
+            </li>
+          </ul>
+        </div>
+      )}
     </React.Fragment>
   );
 };
